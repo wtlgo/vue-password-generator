fcae03bde8968d50acc8cd3f4c3f1abc81465a07
wtlgo <georgeendo0@gmail.com>
2023-02-11 18:30:56 +0300

    Add icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,43 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [VitePWA({ registerType: "autoUpdate" }), vue()],
+    plugins: [
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Password Generator",
+                short_name: "PW",
+                icons: [
+                    {
+                        src: "/favicon.ico",
+                        type: "image/x-icon",
+                        sizes: "16x16 32x32",
+                    },
+                    {
+                        src: "/icon-192.png",
+                        type: "image/png",
+                        sizes: "192x192",
+                    },
+                    {
+                        src: "/icon-512.png",
+                        type: "image/png",
+                        sizes: "512x512",
+                    },
+                    {
+                        src: "/icon-192-maskable.png",
+                        type: "image/png",
+                        sizes: "192x192",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/icon-512-maskable.png",
+                        type: "image/png",
+                        sizes: "512x512",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+        vue(),
+    ],
 });
