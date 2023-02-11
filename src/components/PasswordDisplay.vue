d0b314a92bf0d1823e3b2ed7c4ba0989efc2a05e
wtlgo <georgeendo0@gmail.com>
2023-02-11 17:44:29 +0300

    Add copy to clipboard feature

--- src/components/PasswordDisplay.vue
+++ src/components/PasswordDisplay.vue
@@ -1,4 +1,7 @@
 <style scoped>
+.pointer {
+    cursor: pointer;
+}
 .wrap {
     white-space: pre-wrap;
     word-wrap: break-word;
@@ -6,7 +9,7 @@
 </style>
 
 <template>
-    <div class="alert alert-success">
+    <div class="alert alert-success pointer" @click="onPasswordClick">
         <p class="wrap">{{ password }}</p>
     </div>
 </template>
@@ -16,4 +19,9 @@ import { toRefs } from "vue";
 
 const props = defineProps<{ password: string }>();
 const { password } = toRefs(props);
+
+const onPasswordClick = () => {
+    navigator.clipboard.writeText(password.value);
+    alert("Password was copied to clipboard");
+};
 </script>
