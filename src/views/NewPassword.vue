b161e19b4a75426be88536956dc7fe141d688dc7
wtlgo <georgeendo0@gmail.com>
2023-02-11 14:19:50 +0300

    Add re-generate button

--- src/views/NewPassword.vue
+++ src/views/NewPassword.vue
@@ -15,7 +15,12 @@
         <div class="row">
             <div class="col" />
             <div class="col-auto">
-                <button class="btn btn-warning" @click="onHome">Home</button>
+                <button class="btn btn-warning m-1" @click="onRegenerate">
+                    Re-generate
+                </button>
+                <button class="btn btn-warning m-1" @click="onHome">
+                    Home
+                </button>
             </div>
             <div class="col" />
         </div>
@@ -27,6 +32,7 @@ import { usePasswordStore } from "../store/password";
 import PasswordDisplay from "../components/PasswordDisplay.vue";
 import { useRouter } from "vue-router";
 import { computed, toRefs } from "vue";
+import { useGeneratorStore } from "../store/generator";
 
 const router = useRouter();
 const onHome = () => router.push({ name: "home" });
@@ -34,11 +40,18 @@ const onHome = () => router.push({ name: "home" });
 const props = defineProps<{ id: number }>();
 const { id } = toRefs(props);
 
-const { get } = usePasswordStore();
+const { get, add } = usePasswordStore();
 const password = computed(() => {
     const res = get(id.value);
     if (res) return res;
     onHome();
     return "";
 });
+
+const { generate } = useGeneratorStore();
+const onRegenerate = () => {
+    const password = generate();
+    const id = add(password);
+    router.push({ name: "new-password", params: { id } });
+};
 </script>
