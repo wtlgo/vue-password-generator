a0cdb5050c58964819f77fda26cfbe69cb31bef1
wtlgo <georgeendo0@gmail.com>
2023-02-12 21:11:56 +0300

    Change background color to black

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
             manifest: {
                 name: "Password Generator",
                 short_name: "PG",
+                background_color: "#000000",
                 icons: [
                     {
                         src: "/favicon.ico",
