f5ab274502409512f1da642f40cf7a4a1dda4e86
wtlgo <georgeendo0@gmail.com>
2023-02-12 20:42:19 +0300

    Replace alert with modal

--- src/components/PasswordDisplay.vue
+++ src/components/PasswordDisplay.vue
@@ -12,13 +12,53 @@
     <div class="alert alert-success pointer" @click="onPasswordClick">
         <p class="wrap">{{ password }}</p>
     </div>
+
+    <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="false"
+        ref="modalRef"
+    >
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                        Info
+                    </h1>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">Password was copied to clipboard</div>
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn btn-primary"
+                        data-bs-dismiss="modal"
+                    >
+                        OK
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
-import { toRefs } from "vue";
+import { toRefs, ref } from "vue";
+import { Modal } from "bootstrap";
 
 const props = defineProps<{ password: string }>();
 const { password } = toRefs(props);
+
+const modalRef = ref<HTMLDivElement | null>(null);
 
 const onPasswordClick = async () => {
     try {
@@ -31,6 +71,10 @@ const onPasswordClick = async () => {
     } catch (error) {}
 
     await navigator.clipboard.writeText(password.value);
-    alert("Password was copied to clipboard");
+
+    if (modalRef.value) {
+        const modal = new Modal(modalRef.value);
+        modal.show();
+    }
 };
 </script>
